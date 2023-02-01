c9ef5d7f748028e5618d94b666d3034c4f67e107
pallavicc <pallaviprinces2001@gmail.com>
2023-02-01 14:33:38 +0530

    add

---
+++ src/main/java/com/example/dockerDemo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.dockerDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	String hello() {
+		return "Hello World";
+	}
+	
+
+}
